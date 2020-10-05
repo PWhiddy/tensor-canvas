c0364d944a1be57e0ade70cd7281d5e4db5cfe6e
Peter Whidden <all.cows.like.to.moo@gmail.com>
2020-10-05 00:22:31 -0700

    line implementation

--- tensorcanvas/BaseShapes.py
+++ tensorcanvas/BaseShapes.py
@@ -10,7 +10,14 @@ def make_uv(t):
   uvy = ep.expand_dims(ep.arange(t, 0.0, t.shape[0], 1), axis=0).tile([t.shape[1],1]).transpose()
   return uvx, uvy
 
-def draw_circle(xp, yp, radius, color, t, delta=0.75):
+def dist_to_col(dist, color, blend, t):
+  msk = ep.clip((dist+blend) / (2.0*blend), 0.0, 1.0)
+  msk = msk * msk * (3.0 - 2.0 * msk)
+  msk = msk.expand_dims(axis=2).tile([1,1,3])
+  col_t = ep.astensor(color).expand_dims(axis=0).expand_dims(axis=0).tile([t.shape[0],t.shape[1],1])
+  return msk*t + (1.0-msk)*col_t
+
+def draw_circle(xp, yp, radius, color, t, blend=0.75):
   '''
   Draws a circle onto an image tensor. All units are in pixels
   Parameters:
@@ -18,7 +25,7 @@ def draw_circle(xp, yp, radius, color, t, delta=0.75):
     yp: y position
     radius: circle radius
     color: rgb color tensor with shape (3,) and values in the range 0.0-1.0
-    delta (optional): blending distance
+    blend (optional): blending distance
   Returns:
     tensor with circle drawn onto it
   '''
@@ -26,26 +33,37 @@ def draw_circle(xp, yp, radius, color, t, delta=0.75):
     t = t.permute(1, 2, 0)
   t = ep.astensor(t)
   uvx, uvy = make_uv(t)
-  dist = ((xp-uvx)**2.0 + (yp-uvy)**2.0 + 1.0).sqrt()
-  delta = 0.75
-  msk = ep.clip((dist-(radius-delta)) / (2.0*delta), 0.0, 1.0)
-  msk = msk * msk * (3.0 - 2.0 * msk)
-  msk = msk.expand_dims(axis=2).tile([1,1,3])
-  col_t = ep.astensor(color).expand_dims(axis=0).expand_dims(axis=0).tile([t.shape[0],t.shape[1],1])
-  t = msk*t + (1.0-msk)*col_t
+  dist = ((xp-uvx)**2.0 + (yp-uvy)**2.0 + 1.0).sqrt()-radius
+  t = dist_to_col(dist, color, blend, t)
   t = t.raw
   if type(t) == torch.Tensor:
     t = t.permute(2, 0, 1)
   return t
 
-def draw_line(x1, y1, x2, y2, radius, color):
+def draw_line(x1, y1, x2, y2, radius, color, t, blend=0.75):
   '''
-  iq's implementation
-  float sdSegment( in vec2 p, in vec2 a, in vec2 b )
-  {
-      vec2 pa = p-a, ba = b-a;
-      float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
-      return length( pa - ba*h );
-  }
+  Draws a line onto an image tensor. All units are in pixels
+  Parameters:
+    x1: x position for endpoint 1
+    y1: y position for endpoint 1
+    x2: x position for endpoint 2
+    y2: y position for endpoint 2
+    radius: line width (radius)
+    color: rgb color tensor with shape (3,) and values in the range 0.0-1.0
+    blend (optional): blending distance
+  Returns:
+    tensor with circle drawn onto it
   '''
-  raise NotImplementedError
+  if type(t) == torch.Tensor:
+    t = t.permute(1, 2, 0)
+  t = ep.astensor(t)
+  uvx, uvy = make_uv(t)
+  pax, pay, bax, bay = uvx-x1, uvy-y1, x2-x1, y2-y1
+  h = ep.clip( (pax*bax+pay*bay)/(bax*bax+bay*bay) , 0.0, 1.0)
+  dlx, dly = pax-bax*h, pay-bay*h
+  dist = (dlx*dlx+dly*dly).sqrt()-radius
+  t = dist_to_col(dist, color, blend, t)
+  t = t.raw
+  if type(t) == torch.Tensor:
+    t = t.permute(2, 0, 1)
+  return t
